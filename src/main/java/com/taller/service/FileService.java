1894b44f5d696b4060cb8fc199e1843c15ec6ce2
Efren Ospino <efrenospinojr@gmail.com>
2020-10-19 20:20:23 -0500

    Refactor other file responses to use Response class

--- src/main/java/com/taller/service/FileService.java
+++ src/main/java/com/taller/service/FileService.java
@@ -1,12 +1,13 @@
 package com.taller.service;
 
+import com.taller.dto.ByteFile;
 import com.taller.util.CustomException;
 
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     
-    public byte[] encryptFile(MultipartFile file, String password) throws CustomException;
-    public byte[] decryptFile(MultipartFile file, String password) throws CustomException;
+    public ByteFile encryptFile(MultipartFile file, String password) throws CustomException;
+    public ByteFile decryptFile(MultipartFile file, String password) throws CustomException;
 
 }
--- src/main/java/com/taller/service/FileServiceImpl.java
+++ src/main/java/com/taller/service/FileServiceImpl.java
@@ -1,8 +1,10 @@
 package com.taller.service;
 
+import com.taller.dto.ByteFile;
 import com.taller.security.Security;
 import com.taller.util.CustomException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,10 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileServiceImpl implements FileService {
 
+    @Autowired
+    private Security security;
+
     @Override
-    public byte[] encryptFile(MultipartFile file, String password) throws CustomException {
+    public ByteFile encryptFile(MultipartFile file, String password) throws CustomException {
         try {
-            return new Security().encriptar(file.getBytes(), password);
+            return new ByteFile(security.encriptar(file.getBytes(), password), file.getOriginalFilename());
         } catch (Exception e) {
             throw new CustomException("Archivo no pudo ser encriptado",
 						String.valueOf(HttpStatus.PRECONDITION_FAILED.value()));
@@ -21,9 +26,9 @@ public class FileServiceImpl implements FileService {
     }
 
     @Override
-    public byte[] decryptFile(MultipartFile file, String password) throws CustomException {
+    public ByteFile decryptFile(MultipartFile file, String password) throws CustomException {
         try {
-            return new Security().desencriptar(file.getBytes(), password);
+            return new ByteFile(security.desencriptar(file.getBytes(), password), file.getOriginalFilename());
         } catch (Exception e) {
             throw new CustomException("Archivo no pudo ser desencriptado",
 						String.valueOf(HttpStatus.PRECONDITION_FAILED.value()));
