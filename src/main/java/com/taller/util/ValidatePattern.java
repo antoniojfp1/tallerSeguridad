ff2ebafb8b10b4c0e0b537f533ee651970765030
Antonio Fernandez Paternina <info@antoniofernandez.pro>
2020-10-21 21:04:51 -0500

    Validacion de patron

---
+++ src/main/java/com/taller/util/ValidatePattern.java
@@ -0,0 +1,18 @@
+package com.taller.util;
+
+import java.util.regex.*;
+
+public class ValidatePattern {
+    
+    public boolean isValid(String pass){
+        /// Caracteres, una mayoscula, una minuscula, un numero y un caracter especial
+        Pattern pattern = Pattern.compile("[0-9][a-zA-Z][@#$%^&+=].{8,}");
+        if(!pattern.matcher(pass).matches()){
+            return true;
+        }
+        else{
+            return false;
+        } 
+    }
+
+}
---
+++ src/test/java/com/taller/Util/ValidatePatternTest.java
@@ -0,0 +1,29 @@
+package com.taller.Util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.taller.util.ValidatePattern;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ValidatePatternTest {
+    
+    private static final String PASSWORD = "Jfantonio1.";
+
+    @Autowired
+    private ValidatePattern validation;
+
+    @Test
+    public void vapasswordValid() throws Exception{
+        boolean isValid = validation.isValid(PASSWORD);
+        assertEquals(isValid, true);
+    }
+}
